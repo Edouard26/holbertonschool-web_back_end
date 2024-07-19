096ad3804f208e69308ce40d5be3aa42ad1e94e6
Edouard26 <edouard.halimi@gmail.com>
2024-07-19 11:43:55 +0200

    correction Q4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -5,8 +5,8 @@ export default class Pricing {
     if (typeof amount !== 'number') {
       throw new TypeError('Amount must be a number');
     }
-    if (typeof currency !== 'string') {
-      throw new TypeError('Currency must be a string');
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
     }
     this._amount = amount;
     this._currency = currency;
@@ -14,5 +14,37 @@ export default class Pricing {
 
   get amount() {
     return this._amount;
+  }
+
+  set amount(x) {
+    if (typeof x !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = x;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(x) {
+    if (!(x instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = x;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.symbol})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
   }
 }
