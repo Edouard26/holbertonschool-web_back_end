ac154da9f46f52d30e63d9286711aaceaf649b42
Edouard26 <edouard.halimi@gmail.com>
2024-07-22 12:24:29 +0200

    correction Q1 + Q2

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,6 @@
-export default function getFullResponseFromAPI() {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
+    if ((success)) {
       resolve({
         status: 200,
         body: 'Success',
