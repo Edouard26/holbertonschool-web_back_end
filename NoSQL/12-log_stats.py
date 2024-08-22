8c076896547ce057881390f4ab0abe400bf56fd2
Edouard26 <153003678+Edouard26@users.noreply.github.com>
2024-08-22 04:05:49 +0200

    Q.12 log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,14 +4,20 @@ Provide stats about Nginx logs stored in MongoDB
 '''
 from pymongo import MongoClient
 
-
-if __name__ == '__main__':
+def log_stats():
+n
     x = MongoClient('mongodb://localhost:27017').logs.nginx
+
+
     print(f'{x.count_documents({})} logs')
+
+ 
     print('Methods:')
     print(f'\tmethod GET: {x.count_documents({"method": "GET"})}')
     print(f'\tmethod POST: {x.count_documents({"method": "POST"})}')
     print(f'\tmethod PUT: {x.count_documents({"method": "PUT"})}')
     print(f'\tmethod PATCH: {x.count_documents({"method": "PATCH"})}')
     print(f'\tmethod DELETE: {x.count_documents({"method": "DELETE"})}')
+
+
     print(f'{x.count_documents({"method": "GET", "path": "/status"})} status check')
