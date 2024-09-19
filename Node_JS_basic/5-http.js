c648a1c4f6415ab4c6e1fc86e5e9a39c57f7275f
Edouard26 <edouard.halimi@gmail.com>
2024-09-19 13:07:37 +0200

    Q.5-http.js

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+// Create the HTTP server
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+
+  if (path === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (path === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+
+    const databasePath = process.argv[2];
+    countStudents(databasePath)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+// Listen on port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+// Export the server
+module.exports = app;
