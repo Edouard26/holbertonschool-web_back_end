7b7e5d019821434c8be117c3de6d1d10f847058b
Edouard26 <edouard.halimi@gmail.com>
2024-09-19 11:03:52 +0200

    Q.1 stdin.js + server.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,23 @@
+// Import the readline module
+const readline = require('readline');
+
+// Create an interface to read from stdin and write to stdout
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Print the welcome message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Read the user's input
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
+});
+
+// Print the closing message when the program ends
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
